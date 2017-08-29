e66144dc780385d1e92f227b99ba36b3594d75b8
Leonardo DaVchezt <davchezt@gmail.com>
2017-08-29 14:26:13 +0700

    Update...

--- README.md
+++ README.md
@@ -2,6 +2,5 @@ MySwitch
 ========
 
 esoTalk Skin
-![Preview 1](http://esotalk.org/forum/attachment/54081c3451dca_Untitled%202.png)
 
-![Preview 2](http://esotalk.org/forum/attachment/54081c2c446cc_Untitled.png)
+![Preview](https://raw.githubusercontent.com/davchezt/MySwitch/master/preview.jpg)
--- skin.php
+++ skin.php
@@ -28,11 +28,24 @@ class ETSkin_MySwitch extends ETSkin {
 public function handler_init($sender)
 {
 	// source link
-	$sender->addToMenu('statistics', 'myswitch', '<a href="https://github.com/davchezt/MySwitch">MySwitch Skin</a> desinged by DaVchezt');
 	$sender->addCSSFile((C("esoTalk.https") ? "https" : "http")."://fonts.googleapis.com/css?family=Open+Sans:400,600");
 	$sender->addCSSFile("core/skin/base.css", true);
 	$sender->addCSSFile("core/skin/font-awesome.css", true);
 	$sender->addCSSFile($this->resource("styles.css"), true);
+	// If we're viewing from a mobile browser, add the mobile CSS and change the master view.
+	if ($isMobile = isMobileBrowser()) {
+		$sender->addToMenu('meta', 'myswitch', '<a target="_blank" href="https://github.com/davchezt/MySwitch">MySwitch</a>');
+		$sender->addCSSFile($this->resource("mobile.css"), true);
+		$sender->masterView = "mobile.master";
+		$sender->addToHead("<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0'>");
+	}
+	else {
+		$sender->addToMenu('statistics', 'myswitch', '<a target="_blank" href="https://github.com/davchezt/MySwitch">MySwitch</a> Skin by DaVchezt');
+		if(ET::$session->userId) {
+			$member = ET::memberModel()->getById(ET::$session->get('userId'));
+			$sender->addToMenu("meta", "activeTopic", "<a href='".URL('conversations/all?search=%23contributor%3A'.$member['username'].'%20%2B%20%23active%20today')."'>".T('My active topics')."</a>");
+		}
+	}
 }
 
 }
